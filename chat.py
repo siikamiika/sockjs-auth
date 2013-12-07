918cdc8d4d5740cc4579370840bcb00c3cd81b44
siikamiika <siikamiika@craptop.local>
2013-12-07 09:00:10 +0200

    toimii kai

--- chat.py
+++ chat.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import tornado.ioloop
 import tornado.web
-
 import sockjs.tornado
+import json
 
 
 class IndexHandler(tornado.web.RequestHandler):
@@ -13,30 +13,57 @@ class IndexHandler(tornado.web.RequestHandler):
 
 class ChatConnection(sockjs.tornado.SockJSConnection):
     """Chat connection implementation"""
-    # Tähän tallennetaan paikallaolijat
     participants = set()
 
     def on_open(self, info):
-        #raise tornado.web.HTTPError(403)
         self.info = info
-        self.broadcast(self.participants, self.info.ip+' joined')
-
-        # Add client to the clients list
-        self.participants.add(self)
+        self.auth = False
+        #self.broadcast(self.participants, joininfo)
+        #self.participants.add(self)
 
     def on_message(self, message):
-        # Broadcast message
-        tmp = set(self.participants)
-        tmp.remove(self)
-        self.broadcast(tmp, '{0}: {1}'.format(self.info.ip, message))
+        """
+        Authenticate if not already authenticated.
+        Otherwise broadcast message to participants.
+        """
+        self_set = set()
+        self_set.add(self)
+        try:
+            message = json.loads(message)
+        except:
+            # :gconf:
+            self.broadcast(self_set, 'That doesn\'t taste like JSON')
+            return
+
+        if not self.auth:
+            # Authenticate
+            try:
+                if message['action'] == 'join':
+                    self.nick = message['nick']
+                    self.auth = True
+                    self.broadcast(self.participants, json.dumps(message))
+                    self.participants.add(self)
+                    self.broadcast(self_set, '{"connected": true}')
+            except: # TODO: gottacatchemall
+                pass
+            finally:
+                if not self.auth:
+                    print('auth@{} fail'.format(self.info.ip))
+        elif message.get('body'):
+            # Broadcast message
+            tmp = set(self.participants)
+            tmp.remove(self)
+            msg = {'nick': self.nick, 'body': message.get('body')}
+            self.broadcast(tmp, json.dumps(msg))
+        else:
+            self.broadcast(self_set, 'not a valid message')
 
     def on_close(self):
         # Remove client from the clients list and broadcast leave message
-        try:
-            self.participants.remove(self)
-            self.broadcast(self.participants, self.info.ip+' left')
-        except KeyError:
-            pass
+        self.participants.remove(self)
+        partnotify = {'nick': self.nick, 'action': 'part'}
+        self.broadcast(self.participants, json.dumps(partnotify))
+
 
 if __name__ == "__main__":
     import logging
